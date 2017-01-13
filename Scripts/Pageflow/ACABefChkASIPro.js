a048a385b825ed3bb31d4c7618462899ccab42cc
Tom Pace <tompace@gmail.com>
2017-01-13 11:27:43 -0500

    Update for temp test

--- Scripts/Pageflow/ACABefChkASIPro.js
+++ Scripts/Pageflow/ACABefChkASIPro.js
@@ -17,8 +17,8 @@
 |     will no longer be considered a "Master" script and will not be supported in future releases.  If
 |     changes are made, please add notes above.
 /------------------------------------------------------------------------------------------------------*/
-var showMessage = false; // Set to true to see results in popup window
-var showDebug = false; // Set to true to see debug messages in popup window
+var showMessage = true; // Set to true to see results in popup window
+var showDebug = true; // Set to true to see debug messages in popup window
 var useAppSpecificGroupName = false; // Use Group name when populating App Specific Info Values
 var useTaskSpecificGroupName = false; // Use Group name when populating Task Specific Info Values
 var cancel = false;
@@ -81,7 +81,7 @@ try {
 	loadAppSpecific4ACA(AInfo); // Add AppSpecific Info
 
 	// page flow custom code begin
-
+aa.print("Do you have a clue?");
 	var allContractorsExist = true;
 	var lpTypes = new Array();
 	var commentText = "Each permit requires a specified contractor license. Please add all required contractor types to the license professional list. This application requires the following contractors to be in the list. ";
