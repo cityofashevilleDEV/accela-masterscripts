a4b248d58cbc83d56c63a2dbbc4b32656804acb3
Tom Pace <tompace@gmail.com>
2017-02-10 10:42:24 -0500

    Update WTUB;~!~!~!~.js
    Adding check for fees due (invoiced and non-invoiced)

--- Scripts/Event/WTUB;~!~!~!~.js
+++ Scripts/Event/WTUB;~!~!~!~.js
@@ -17,6 +17,24 @@ if (matches(wfStatus, 'Issue', 'Reissue', 'Issue Partial') && (isTaskActive('HRC
 	cancel = true;
 }
 
+////Checking for fees due (invoiced & non-invoiced) and if so, stopping a permit from being issued internally
+//If there are invoiced fees (balance due), we should not be able to issue a permit (internally)
+if (matches(wfStatus, 'Issue', 'Reissue', 'Issue Partial')) {
+	if (appMatch('Permits/*/*/*') && balanceDue > 0) {
+	                showMessage = true;
+	                comment('<font size=small><b>Balance Due:</b></font><br><br>You will need to pay the Balance Due before a permit can be issued.<br><br>');
+	                cancel = true;
+	}
+	//If there are non-invoiced fees, we should not be able to issue a permit (internally)
+	// Note from Keith H -- Check if there a Assessed fees:    Check for NON Invoiced fees
+	                newFees = feeGetTotByDateRange2("01/01/2000","01/01/2030","NEW"); 
+	                comment("NEW fee total = "+newFees); comment("Workflow status = "+wfStatus);
+	if (appMatch('Permits/*/*/*') && newFees > 0) {
+	                showMessage = true; comment("<font size=small><b>Un-Invoiced Fees:</b></font><br><br>The permit you are trying to issue or final has assessed fees that have NOT been invoiced. Those fees need to be invoiced or removed prior to the permit being issued or finaled.<br><br>");
+	                cancel = true;
+	}
+}
+
 if (wfTask == 'Building' && matches(wfStatus, 'Approved', 'Approved with Conditions', 'Partial Approval') && (getPriority() == null || getPriority() == '')) {
 	cancel = true;
 	showMessage = true;
